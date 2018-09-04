a05dc1034086b138a0425f3793b8c4acde04a59a
Matthew Boynes <mboynes@alley.co>
2018-09-04 17:07:00 -0400

    Initial commit

---
+++ csv-import-framework.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     CSV Import Framework
+ * Plugin URI:      https://github.com/alleyinteractive/csv-import-framework/
+ * Description:     A framework for building custom CSV imports
+ * Author:          Alley Interactive
+ * Author URI:      https://alley.co
+ * Text Domain:     csv-import-framework
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         CSV_Import_Framework
+ */
+
+namespace CSV_Import_Framework;
